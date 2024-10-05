2e900ad8a5e1bf5efe82506d3d4d0a391ff97156
majorBien <shadow7.dev@gmail.com>
2024-10-05 10:31:05 +0200

    lora task create

---
+++ main/tasks_settings.h
@@ -0,0 +1,20 @@
+/*
+ * tasks_settings.h
+ *
+ *  Created on: 5 paź 2024
+ *      Author: majorbien
+ */
+
+#ifndef MAIN_TASKS_SETTINGS_H_
+#define MAIN_TASKS_SETTINGS_H_
+
+
+
+// LoraWAN Send task
+#define LORA_SEND_STACK_SIZE				4096
+#define LORA_SEND_PRIORITY					5
+#define LORA_SEND_CORE_ID					0
+
+
+
+#endif /* MAIN_TASKS_SETTINGS_H_ */
